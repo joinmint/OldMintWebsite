cd845dfb7b10f66abce6afeaf7a5d2fcf00f4096
joinmint <59270777+joinmint@users.noreply.github.com>
2019-12-29 18:42:37 -0500

    replaced contact page code test and email php

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,70 +1,27 @@
-﻿<?php
-
-// Replace this with your own email address
-$siteOwnersEmail = 'mcw1000@yahoo.com';
-
-
-if($_POST) {
-
-    $name = trim(stripslashes($_POST['contactName']));
-    $email = trim(stripslashes($_POST['contactEmail']));
-    $subject = trim(stripslashes($_POST['contactSubject']));
-    $contact_message = trim(stripslashes($_POST['contactMessage']));
-
-    // Check Name
-    if (strlen($name) < 2) {
-        $error['name'] = "Please enter your name.";
-    }
-    // Check Email
-    if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
-        $error['email'] = "Please enter a valid email address.";
-    }
-    // Check Message
-    if (strlen($contact_message) < 15) {
-        $error['message'] = "Please enter your message. It should have at least 15 characters.";
-    }
-    // Subject
-    if ($subject == '') { $subject = "Contact Form Submission"; }
-
-
-    // Set Message
-    $message .= "Email from: " . $name . "<br />";
-    $message .= "Email address: " . $email . "<br />";
-    $message .= "Message: <br />";
-    $message .= $contact_message;
-    $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
-
-    // Set From: header
-    $from =  $name . " <" . $email . ">";
-
-    // Email Headers
-    $headers = "From: " . $from . "\r\n";
-    $headers .= "Reply-To: ". $email . "\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n"
-    $headers .=  "X-mailer: PHP/" . phpversion () ;
-    $mail = mail($siteOwnersEmail, $subject, $message, $headers);
-
-    if (!$error) {
-
-        ini_set("sendmail_from", $siteOwnersEmail); // for windows server
-        $mail = mail($siteOwnersEmail, $subject, $message, $headers);
-
-        if ($mail) { echo "OK"; }
-        else { echo "Something went wrong. Please try again."; }
-
-    } # end if - no validation error
-
-    else {
-
-        $response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
-        $response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
-        $response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
-
-        echo $response;
-
-    } # end if - there was a validation error
-
+<?php
+$field_name = $_POST['cf_name'];
+$field_email = $_POST['cf_email'];
+$field_message = $_POST['cf_message'];
+$mail_to = 'youremail@emaildomain.com';
+$subject = 'Your Subject'.$field_name;
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Message: '.$field_message;
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Sucess Message');
+		window.location = 'contact.html';
+	</script>
+<?php
 }
-
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Error Message');
+		window.location = 'contact.html';
+	</script>
+<?php
+}
 ?>
